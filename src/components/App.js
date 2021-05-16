aa65421e3af7223a44c700c1b9f3bbe68929be3c
abhikaushik <abhikaushik@paypal.com>
2021-05-17 04:11:45 +0530

    changed list type to letters

--- src/components/App.js
+++ src/components/App.js
@@ -50,7 +50,7 @@ class App extends Component {
     //const uniqueCities = [...new Set(validCities)];
     //console.log(uniqueCities);
     const orderedList = validCities.map((city) => (
-      <li type="1" key={`location${city.key}`}>
+      <li type="a" key={`location${city.key}`}>
         {city.name}
       </li>
     ));
